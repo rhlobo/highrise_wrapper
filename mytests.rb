f76f51e0007e6691b77468e03088d85bd2fcffa8
rhlobo <rhlobo+github@gmail.com>
2014-03-17 19:29:54 -0300

    Improving tests to learn about the api.

--- mytests.rb
+++ mytests.rb
@@ -8,15 +8,20 @@ Highrise::Base.format = :xml
 @people = Highrise::Person.find_all_across_pages
 
 # [#<Highrise::Person:0x9db1d80 @attributes={"author_id"=>1018065, "background"=>nil, "company_id"=>nil, "created_at"=>2014-03-17 22:11:22 UTC, "first_name"=>"A", "group_id"=>nil, "id"=>201374388, "last_name"=>"A", "owner_id"=>nil, "title"=>nil, "updated_at"=>2014-03-17 22:11:22 UTC, "visible_to"=>"Everyone", "company_name"=>nil, "linkedin_url"=>nil, "avatar_url"=>"http://dge9rmgqjs8m1.cloudfront.net/highrise/missing/avatar.gif?r=3", "contact_data"=>#<Highrise::Person::ContactData:0x9db0a98 @attributes={"instant_messengers"=>[], "twitter_accounts"=>[], "email_addresses"=>[], "phone_numbers"=>[], "addresses"=>[], "web_addresses"=>[]}, @prefix_options={}, @persisted=true>, "subject_datas"=>[#<Highrise::Person::SubjectData:0x9dafc88 @attributes={"id"=>118062439, "subject_field_id"=>878834, "subject_field_label"=>"Customer ID", "value"=>"A"}, @prefix_options={}, @persisted=true>]}, @prefix_options={}, @persisted=true>]
-p @people.methods
 
 
 class Contact
 	attr_accessor :name, :last_name, :email, :company, :job_title, :phone, :website
 
-	def initialize
+	def initialize site, user, attributes
+		@site = site
+		@user = user
+		attributes.each { |k, v| instance_variable_set("@#{k}", v) }
 	end
 
 	def save
 	end
 end
+
+c = Contact.new "site", "user", {name: "fernando"}
+puts c.name
