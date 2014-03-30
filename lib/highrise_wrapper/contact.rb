a0b7ced9528b8ae008ab1bc90b2f76c1e5bc7adf
rhlobo <rhlobo+github@gmail.com>
2014-03-30 19:28:49 -0300

    Cleaning up.

--- lib/highrise_wrapper/contact.rb
+++ lib/highrise_wrapper/contact.rb
@@ -1,22 +1,15 @@
 module Highrise_wrapper
 
 	module InstanceMethods
-	  def save_to_highrise
-	  	self.company.highrise_base_url
+	  def highrise_save
+	  	puts 'Saving'
+	  	sleep(10.seconds)
 	  end
-	end
 
-	class Contact
-		attr_accessor :name, :last_name, :email, :company, :job_title, :phone, :website
-
-		def initialize site, user, attributes
-			@site = site
-			@user = user
-			attributes.each { |k, v| instance_variable_set("@#{k}", v) }
-		end
-
-		def save
-		end
+	  def highrise_remove
+	  	puts 'Removing'
+	  	sleep(10.seconds)
+	  end
 	end
 
 end
